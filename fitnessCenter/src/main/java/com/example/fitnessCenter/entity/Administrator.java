4dcfbdd80ba378424c8d10de04004c420ee64f1b
mbiljana <mbiljana99@gmail.com>
2021-06-02 18:42:48 +0200

    Izmene modela

--- fitnessCenter/src/main/java/com/example/fitnessCenter/entity/Administrator.java
+++ fitnessCenter/src/main/java/com/example/fitnessCenter/entity/Administrator.java
@@ -1,6 +1,10 @@
 package com.example.fitnessCenter.entity;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+
 import javax.persistence.Entity;
+
 
 @Entity
 public class Administrator extends Korisnik{
