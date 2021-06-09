cb6dcc785eb13681daad27b3247b300d2ccbb539
mbiljana <mbiljana99@gmail.com>
2021-06-09 22:22:24 +0200

    commit

--- fitnessCenter/src/main/resources/static/js/registracijaTrenera.js
+++ fitnessCenter/src/main/resources/static/js/registracijaTrenera.js
@@ -1,20 +1,39 @@
-$.ajax({
-    type: "GET",
-    url: "http://localhost:8090/api/korisnik/zahteviZaRegistraciju",
-    dataType: "json",
-    success: function (response) {
-        console.log("SUCCESS:\n", response);
-        for (let trener of response) {
-            let row = "<tr>";
-            row += "<td>" + trener.ime + "</td>";
-            row += "<td>" + trener.prezime + "</td>";
-            row += "<td>" + trener.email + "</td>";
-            row += "<td>" + trener.telefon + "</td>";
-            row += "</tr>";
+$(document).ready(function() {
 
-            $('#zahtevi').append(row);
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:8090/api/korisnik/registrujTrenera",
+        dataType: "json",
+        contentType: "application/json",
+        data: obj,
+        success: function () {
+            alert(obj);
+            window.location.href = "pregledZahtevaZaRegTrenera.html";
+        },
+        error: function (data) {
+            alert("Greska");
         }
-    },
-    error: function (response) {
-    }
+    });
+
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8090/api/korisnik/zahteviZaRegistraciju",
+        dataType: "json",
+        success: function (response) {
+            console.log("SUCCESS:\n", response);
+            for (let trener of response) {
+                let row = "<tr>";
+                row += "<td>" + trener.ime + "</td>";
+                row += "<td>" + trener.prezime + "</td>";
+                row += "<td>" + trener.email + "</td>";
+                row += "<td>" + trener.telefon + "</td>";
+                row += "</tr>";
+
+                $('#zahtevi').append(row);
+            }
+        },
+        error: function (response) {
+        }
+    });
 });
